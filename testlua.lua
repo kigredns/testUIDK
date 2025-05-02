2021751bdca2e7009cd78d561b903c73ff874d73
SanderDevMaybe <mikolajkim585@gmail.com>
2025-05-02 12:59:46 +0200

    Update testlua.lua

--- testlua.lua
+++ testlua.lua
@@ -583,13 +583,13 @@ local rankData = {
 
 local player = game.Players.LocalPlayer
 local rank = getRank(player)
-local data = rankData[rank] or {color = "rgb(255,255,255)", icon = "?"}
+local data = rankData[rank] or {color = "rgb(174, 255, 1)", icon = ""}
 
 local fullText = "<font color='rgb(0, 255, 255)'>Sander</font> " ..
                  "<font color='rgb(255, 255, 255)'>XY「 BrookHaven 」 • </font>" ..
                  "<font color='rgb(24, 255, 185)'>EASTER</font> " ..
                  "<font color='rgb(255, 255, 255)'> • </font>" ..
-                 string.format("<font color='%s'>%s | %s |</font>", data.color, data.icon, rank)
+                 string.format("<font color='%s'>%s %s </font>", data.color, data.icon, rank)
 
 local WindowName = SetProps(MakeElement("Label", WindowConfig.Name, 14), {
     Size = UDim2.new(1, -30, 2, 0),
