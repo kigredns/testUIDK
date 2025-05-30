d6821a8129f351e1462efa93bb96db78c0db6712
SanderDevMaybe <mikolajkim585@gmail.com>
2025-05-30 23:04:22 +0200

    Update SanderXYUI.lua

--- SanderXYUI.lua
+++ SanderXYUI.lua
@@ -587,10 +587,10 @@ local data = rankData[rank] or {color = "rgb(174, 255, 1)", icon = ""}
 
 local fullText = "<font color='rgb(0, 255, 255)'>Sander</font> " ..
                  "<font color='rgb(255, 255, 255)'>XY「 BrookHaven 」 • </font>" ..
-                 "<font color='rgb(24, 255, 185)'>Spring</font> " ..
+                 "<font color='rgb(255, 120, 0)'>Vacation Coming!</font> " ..
                  "<font color='rgb(255, 255, 255)'> • </font>" ..
                  string.format("<font color='%s'>%s %s </font>", data.color, data.icon, rank)
-
+			
 local WindowName = SetProps(MakeElement("Label", WindowConfig.Name, 14), {
     Size = UDim2.new(1, -30, 2, 0),
     Position = UDim2.new(0, 25, 0, -24),
