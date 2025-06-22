1f11a819a490e65ae708b90fb7c0f4a530a47970
SanderDevMaybe <mikolajkim585@gmail.com>
2025-06-22 21:50:37 +0200

    Update DuplicateInfo.lua

--- DuplicateInfo.lua
+++ DuplicateInfo.lua
@@ -22,8 +22,8 @@ Frame.BackgroundTransparency = 0.2
 Frame.BorderColor3 = Color3.fromRGB(0, 0, 0)
 Frame.BorderSizePixel = 0
 Frame.Size = UDim2.new(0, 452, 0, 278)
-Frame.Position = UDim2.new(0.5, 0, 0.5, 0) -- Wyśrodkowanie
-Frame.AnchorPoint = Vector2.new(0.5, 0.5) -- Środek ramki na środku ekranu
+Frame.Position = UDim2.new(0.5, 0, 0.5, 0) 
+Frame.AnchorPoint = Vector2.new(0.5, 0.5) 
 
 UICorner.CornerRadius = UDim.new(0, 16)
 UICorner.Parent = Frame
@@ -54,24 +54,22 @@ TextLabel_3.BackgroundTransparency = 1.0
 TextLabel_3.Position = UDim2.new(0.05, 0, 0.2, 0)
 TextLabel_3.Size = UDim2.new(0.9, 0, 0.6, 0)
 TextLabel_3.Font = Enum.Font.SourceSansBold
-TextLabel_3.Text = "You are duplicating items. Be patient."
+TextLabel_3.Text = "You are now getting items, be patient (New System!)"
 TextLabel_3.TextColor3 = Color3.fromRGB(255, 255, 255)
 TextLabel_3.TextScaled = true
 TextLabel_3.TextWrapped = true
 
--- Animacja ruchu góra-dół
+
 local function animateUI()
     while true do
-        -- Pozycja początkowa
+      
         local upPosition = UDim2.new(0.5, 0, 0.48, 0)
         local downPosition = UDim2.new(0.5, 0, 0.52, 0)
-        
-        -- Stworzenie tweenu
+
         local tweenInfo = TweenInfo.new(0.4, Enum.EasingStyle.Sine, Enum.EasingDirection.InOut)
         local tweenUp = TweenService:Create(Frame, tweenInfo, {Position = upPosition})
         local tweenDown = TweenService:Create(Frame, tweenInfo, {Position = downPosition})
 
-        -- Wykonanie animacji
         tweenUp:Play()
         tweenUp.Completed:Wait()
         tweenDown:Play()
@@ -79,5 +77,4 @@ local function animateUI()
     end
 end
 
--- Uruchomienie animacji
 task.spawn(animateUI)
