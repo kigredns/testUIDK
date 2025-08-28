f058486f4fa885194298083a84b8553a747a4530
SanderDevMaybe <mikolajkim585@gmail.com>
2025-08-28 16:58:22 +0200

    Update CloseDuplicateInfo.lua

--- CloseDuplicateInfo.lua
+++ CloseDuplicateInfo.lua
@@ -1,12 +1,5 @@
 local player = game.Players.LocalPlayer
-local playerGui = player:WaitForChild("PlayerGui")
-
--- Find the GUI by its name ("ScreenGui")
-local screenGui = playerGui:FindFirstChild("ScreenGui")
-
-if screenGui then
-    screenGui:Destroy() -- Remove the GUI
-    print("ScreenGui has been removed.")
-else
-    print("ScreenGui not found.")
+local gui = player:WaitForChild("PlayerGui"):FindFirstChild("ScreenGui")
+if gui then
+gui:Destroy()
 end
