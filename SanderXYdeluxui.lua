a8ce9850897ffe2e0c5a07245f289ed187e2fbe4
SanderDevMaybe <mikolajkim585@gmail.com>
2025-10-12 21:52:50 +0200

    Update SanderXYdeluxui.lua

--- SanderXYdeluxui.lua
+++ SanderXYdeluxui.lua
@@ -587,7 +587,7 @@ local data = rankData[rank] or {color = "rgb(174, 255, 1)", icon = ""}
 
 local fullText = "<font color='rgb(0, 255, 255)'>Sander</font> " ..
                  "<font color='rgb(255, 255, 255)'>XY「 BrookHaven 」 • </font>" ..
-                 "<font color='rgb(79, 184, 37)'>BACK TO SCHOOL</font> " ..
+                 "<font color='rgb(1, 0.5, 0)'>Halloween 🎃</font> " ..
                  "<font color='rgb(255, 255, 255)'> • </font>" ..
                  string.format("<font color='%s'>%s %s </font>", data.color, data.icon, rank)
 
